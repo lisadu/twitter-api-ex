3f464cdcd47842b283353348cdf5cda079c3de80
Lisa Du <lisa@futureadvisor.com>
2015-05-10 21:15:08 -0700

    Only render some tweet fields Add with_picture filter

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,8 +8,18 @@ class TweetsController < ApplicationController
     if params[:until]
       query += " since:#{params[:until]}"
     end
+    if params[:with_picture] == '1'
+      query += " filter:images"
+    end
     begin
-      @tweets = @client.search(query)
+      @tweets = @client.search(query).map do |tweet|
+        {
+            :retweet_count => tweet.retweet_count,
+            :text => tweet.text,
+            :created_at => tweet.created_at,
+            :media => tweet.media? ? tweet.media.map{|m| m.uri.to_s} : nil
+        }
+      end
       render json: @tweets
     rescue Twitter::Error
       render json: { message: 'User not found' }, status: :not_found
